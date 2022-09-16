ac7d38b006b69c35391068a7c56104b77d53a173
ProMix0 <misha.zavertkin@mail.ru>
2022-09-17 00:04:33 +0700

    Add resolving hosted service in scope

---
+++ Utils/ResolveInScope.cs
@@ -0,0 +1,25 @@
+using BetterHostedServices;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Utils
+{
+    public class ResolveInScope<T> : NotEndingBackgroundService
+    where T : CriticalBackgroundService
+    {
+        private IServiceProvider provider;
+
+        public ResolveInScope(IServiceProvider provider)
+        {
+            this.provider = provider;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            using IServiceScope scope = provider.CreateScope();
+
+            T worker = ActivatorUtilities.CreateInstance<T>(provider);
+
+            await worker.StartAsync(stoppingToken);
+        }
+    }
+}
